6891d41dbc121c5e41ce2aaf027ea06e06b52f38
EdisonGL <94578516+EdisonGL@users.noreply.github.com>
2022-10-24 07:55:18 -0500

    Codigo y carpetas innecesarias

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesion</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body class="fondo">
+    <div class="container">
+        <div class="text-center card car-w col-md-6 offset-md-3">
+            <h1>Inicio de sesion</h1>
+            <br>
+            <form action="">
+            <input class="form-control int-p" type="text" placeholder="Nombre de Usuario">
+            <br>
+            <input class="form-control int-p" type="password" placeholder="Contraseña" >
+            <a href="./registro.php">registrarse</a>
+            <br>
+            <button type="sumit" class="btn btn-primary">Iniciar sesion</button>
+            <br>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse </title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body class="cl-fondo">
+    <form action="">
+        <div align="center" class="container">
+            <div class="row card text-center">
+                <h1>Registro</h1>
+                <label class="col-md-6 offset-md-3" for="">Nombres</label>
+                <div class="col-md-6 col-lg-6 col-xl-6 offset-md-3">
+                    <input class="form-control" type="text">
+                </div>
+                <br>
+                <label class="col-md-6 offset-md-3" for="">Apellidos</label>
+                <div class="text-center col-md-6 col-lg-6 col-xl-6 offset-md-3">
+                    <input class="form-control" type="text">
+                </div>
+                <br>
+                <label class="col-md-6 offset-md-3" for="">Número de Celular/Telefóno</label>
+                <div class="text-center col-md-6 col-lg-6 col-xl-6 offset-md-3">
+                    <input class="form-control" type="number">
+                </div>
+                <br>
+                <label class="col-md-6 offset-md-3" for="">Correo Electronico</label>
+                <div class="text-center col-md-6 col-lg-6 col-xl-6 offset-md-3">
+                    <input class="form-control" type="email">
+                </div>
+                <br>
+                <label class="col-md-6 offset-md-3" for="">Contraseña</label>
+                <div class="text-center col-md-6 col-lg-6 col-xl-6 offset-md-3">
+                    <input class="form-control" type="text">
+                </div>
+                <br>
+                <label class="col-md-6 offset-md-3" for="">Confirmar contraseña</label>
+                <div class="text-center col-md-6 col-lg-6 col-xl-6 offset-md-3">
+                    <input class="form-control" type="text">
+                </div>
+                <br>
+                <button class="btn btn-primary" type="sumit">Crear usuario</button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
